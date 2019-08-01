742bbc56f68375e579343339b23bd9e10b9f530f
jvme <juanv.mari@gmail.com>
2019-08-01 23:00:20 +0200

    Camera problem workarround

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @NgModule({
   imports: [
@@ -16,23 +15,6 @@ import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
   declarations: [Tab2Page]
 })
 export class Tab2PageModule {
-  currentImage: any;
+  constructor() {}
 
-  constructor(private camera: Camera) { }
-
-  takePicture() {
-    const options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.DATA_URL,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    }
-
-    this.camera.getPicture(options).then((imageData) => {
-      this.currentImage = 'data:image/jpeg;base64,' + imageData;
-    }, (err) => {
-     // Handle error
-     console.log("Camera issue:" + err);
-    });
-  }
 }
